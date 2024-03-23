099aef61e2350b05db56a8eb2416431e1ec5e42b
Jihoon Park <bartleby2718@gmail.com>
2024-03-23 19:06:33 -0400

    Minimal reproducible example for appveyor/ci#3914

--- MyProject.Tests/UnitTest1.cs
+++ MyProject.Tests/UnitTest1.cs
@@ -7,9 +7,45 @@ public class Tests
     {
     }
 
-    [Test]
-    public void Test1()
+    [TestCase("localhost")]
+    [TestCase(@"(local)\SQL2017")]
+    public void MicrosoftDataSqlClientTest(string dataSource)
     {
-        Assert.Pass();
+        var connectionString = new Microsoft.Data.SqlClient.SqlConnectionStringBuilder
+        {
+            DataSource = dataSource,
+            UserID = "sa",
+            Password = "Password12!",
+            TrustServerCertificate = true,
+        }.ConnectionString;
+
+        using var connection = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
+        connection.Open();
+        using var command = new Microsoft.Data.SqlClient.SqlCommand("SELECT 1", connection);
+        if (command.ExecuteScalar() is { } result)
+        {
+            Console.WriteLine($"The result is {result}");
+        }
+    }
+
+    [TestCase("localhost")]
+    [TestCase(@"(local)\SQL2017")]
+    public void SystemDataSqlClientTest(string dataSource)
+    {
+        var connectionString = new System.Data.SqlClient.SqlConnectionStringBuilder
+        {
+            DataSource = dataSource,
+            UserID = "sa",
+            Password = "Password12!",
+            TrustServerCertificate = true,
+        }.ConnectionString;
+
+        using var connection = new System.Data.SqlClient.SqlConnection(connectionString);
+        connection.Open();
+        using var command = new System.Data.SqlClient.SqlCommand("SELECT 1", connection);
+        if (command.ExecuteScalar() is { } result)
+        {
+            Console.WriteLine($"The result is {result}");
+        }
     }
 }
